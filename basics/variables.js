04d198ad631bfcf86ae673082fb55ba0141dbf55
Devyansh Sharma <129656658+devyansh-007@users.noreply.github.com>
2025-01-04 10:09:51 +0000

    added new file

---
+++ basics/variables.js
@@ -0,0 +1,8 @@
+const accountId = 144343
+let accountEmail = "devyanshshamra4555@google.com"
+var accountPassword = "123454321"
+accountCity = "Jaipur"
+
+// accountId = 2
+accountEmail = "hc&wjdbd"
+console.table([accountEmail,accountId,accountPassword,accountCity]);
